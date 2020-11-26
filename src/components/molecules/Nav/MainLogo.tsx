acd3b5e2a59a1741f7d55a23de5de97fd7ac1900
gunwooko <kkw722@gmail.com>
2020-11-26 23:12:44 +0900

    [:package:] uninstalled node-sass && installed node-sass 4.14.1

--- src/components/molecules/Nav/MainLogo.tsx
+++ src/components/molecules/Nav/MainLogo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import javascript from 'components/img/javascript.png';
+import './MainLogo.scss';
 
 const MainLogo: React.FunctionComponent = () => (
-  <>
+  <div className="mainLogoComponent">
     <label>
       <img src={javascript} alt="mainLogo" width="40px" height="40px"></img>
       <p>깔깔스페인어</p>
     </label>
-  </>
+  </div>
 );
 
 export default MainLogo;
