1afff923bec1dbb630acd54965cff3f5e233b170
gunwooko <kkw722@gmail.com>
2020-11-30 17:51:34 +0900

    [:nail_care: :hammer:] completed nav view, npm run eject to config scss

--- src/components/organims/Nav.tsx
+++ src/components/organims/Nav.tsx
@@ -3,14 +3,15 @@ import ClassButton from 'components/molecules/Nav/ClassButton';
 import MainLogo from 'components/molecules/Nav/MainLogo';
 import NavMenu from 'components/molecules/Nav/NavMenu';
 import React from 'react';
+import './Nav.scss';
 
 const Nav: React.FunctionComponent = () => (
-  <>
+  <div className="Nav">
     <MainLogo></MainLogo>
     <NavMenu></NavMenu>
     <AuthButton></AuthButton>
     <ClassButton></ClassButton>
-  </>
+  </div>
 );
 
 export default Nav;
