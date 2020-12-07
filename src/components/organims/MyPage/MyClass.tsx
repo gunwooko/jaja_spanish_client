32e0169b71545c2ac320546d7db214f5a5e7f55c
gunwooko <kkw722@gmail.com>
2020-12-07 17:42:33 +0900

    [:hammer:] add classData, update MyClass, MyDetails

--- src/components/organims/MyPage/MyClass.tsx
+++ src/components/organims/MyPage/MyClass.tsx
@@ -1,9 +1,40 @@
+import Div from 'components/atoms/Div';
 import React from 'react';
 
-const MyClass: React.FunctionComponent = () => (
-  <div>
-    <h2>MyClass</h2>
-  </div>
+interface Props {
+  teachers_name: string;
+  class_status: string;
+  class_style: string;
+  class_lastDay: string;
+}
+
+const MyClass: React.FunctionComponent<Props> = ({
+  teachers_name,
+  class_status,
+  class_style,
+  class_lastDay,
+}: Props) => (
+  <Div className="MyClass">
+    <Div className="myClass_title" text="나의 강의"></Div>
+    <Div className="myClass_box">
+      <Div className="myClass_row">
+        <span className="myClass_subtitle">강사이름</span>
+        <span className="myClass_info">{teachers_name}</span>
+      </Div>
+      <Div className="myClass_row">
+        <span className="myClass_subtitle">수업상태</span>
+        <span className="myClass_info">{class_status}</span>
+      </Div>
+      <Div className="myClass_row">
+        <span className="myClass_subtitle">수업유형</span>
+        <span className="myClass_info">{class_style}</span>
+      </Div>
+      <Div className="myClass_row">
+        <span className="myClass_subtitle">수업종료일</span>
+        <span className="myClass_info">{class_lastDay}</span>
+      </Div>
+    </Div>
+  </Div>
 );
 
 export default MyClass;
---
+++ src/testData/classData.ts
@@ -0,0 +1,8 @@
+const classData = {
+  teachers_name: 'Pablo',
+  class_status: '등록',
+  class_style: '주 4회 28분',
+  class_lastDay: '2020.11.30',
+};
+
+export default classData;
