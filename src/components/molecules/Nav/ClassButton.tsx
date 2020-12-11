d0d69cd0eef673b3dc9f2ce149f2ab31cd95b334
gunwooko <kkw722@gmail.com>
2020-12-11 13:42:28 +0900

    [:hammer:] add alert function to 카톡상담필요한 부분 - Nav: ClassButton, - Home: FourthViewDiv=>  상담하기 버트 - Footer: FooterInfo => 카톡버튼, - Community: FaqLastView => 1:1 상담 버튼

--- src/components/molecules/Nav/ClassButton.tsx
+++ src/components/molecules/Nav/ClassButton.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ClassButton.scss';
 
-const ClassButton: React.FunctionComponent = () => (
-  <div className="classButton">
-    <Link to="/" style={{ textDecoration: 'none' }}>
-      <button>수업신청</button>
-    </Link>
-  </div>
-);
+const ClassButton: React.FunctionComponent = () => {
+  return (
+    <div className="classButton">
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <button>수업신청</button>
+      </Link>
+    </div>
+  );
+};
 
 export default ClassButton;
