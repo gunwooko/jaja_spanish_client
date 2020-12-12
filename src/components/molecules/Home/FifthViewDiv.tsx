4a95fb85d5b571870267fb01a471fe10b0cc0b5b
gunwooko <kkw722@gmail.com>
2020-12-12 13:46:12 +0900

    [:sparkles: :hammer:] Add userName, password verification, add authAlert

--- src/components/molecules/Home/FifthViewDiv.tsx
+++ src/components/molecules/Home/FifthViewDiv.tsx
@@ -1,22 +1,39 @@
 import Div from 'components/atoms/Div';
+import useLoggedInCheck from 'Hooks/useLoggedInCheck';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './FifthViewDiv.scss';
 
-const FifthViewDiv: React.FunctionComponent = ({}) => (
-  <div className="FifthViewDiv">
-    <Div className="fifth_vw_1">
-      저렴한 가격의 시범강의로
-      <br /> 부담없이 시작해보세요.
-    </Div>
-    <Div className="fifth_vw_2">
-      말하고 바로 피드백 받자
-      <br />
-      1:1 화상 스페인어
-    </Div>
-    <Link to="/submitclass">
-      <button className="fifth_vw_btn">지금 시작하기</button>
-    </Link>
-  </div>
-);
+const FifthViewDiv: React.FunctionComponent = ({}) => {
+  const isLoggedIn = useLoggedInCheck();
+
+  const alertNeededAuth = () => {
+    alert('로그인 후 이용할 수 있습니다.');
+  };
+
+  return (
+    <div className="FifthViewDiv">
+      <Div className="fifth_vw_1">
+        저렴한 가격의 시범강의로
+        <br /> 부담없이 시작해보세요.
+      </Div>
+      <Div className="fifth_vw_2">
+        말하고 바로 피드백 받자
+        <br />
+        1:1 화상 스페인어
+      </Div>
+      {isLoggedIn ? (
+        <Link to="/submitclass">
+          <button className="fifth_vw_btn">지금 시작하기</button>
+        </Link>
+      ) : (
+        <Link to="/login" style={{ textDecoration: 'none' }}>
+          <button onClick={alertNeededAuth} className="fifth_vw_btn">
+            지금 시작하기
+          </button>
+        </Link>
+      )}
+    </div>
+  );
+};
 export default FifthViewDiv;
