160fb06fc61d757896af7387466900d25696680d
gunwooko <kkw722@gmail.com>
2020-12-15 14:13:56 +0900

    [:nail_care:] MyPage Styles

--- src/components/organims/MyPage/ApplyToPostponeClass.tsx
+++ src/components/organims/MyPage/ApplyToPostponeClass.tsx
@@ -2,27 +2,37 @@ import Div from 'components/atoms/Div';
 import React from 'react';
 
 const ApplyToPostponeClass: React.FunctionComponent = () => (
-  <Div className="MyPaymentDetails">
-    <Div className="myPayment_title" text="수업연기신청"></Div>
-    <Div className="myPayment_box">
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">취소하는 수업</span>
-        <i className="far fa-calendar-alt"></i>
-        <input type="date" placeholder="날짜선택"></input>
-        <i className="far fa-clock"></i>
-        <input type="time" name="time" />
+  <Div className="ApplyToPostponedClass_form">
+    <div></div>
+    <Div className="ApplyToPostponeClass">
+      <Div className="ApplyToPostponeClass_title" text="수업연기신청"></Div>
+      <Div className="ApplyToPostponeClass_box">
+        <Div className="ApplyToPostponeClass_row">
+          <span className="ApplyToPostponeClass_subtitle">취소하는 수업</span>
+          {/* <i className="far fa-calendar-alt"></i> */}
+          <input className="applyToPostponeInput_date" type="date" placeholder="날짜선택" />
+          {/* <i className="far fa-clock"></i> */}
+          <input className="applyToPostponeInput_date" type="time" name="time" />
+        </Div>
+        <Div className="ApplyToPostponeClass_row">
+          <span className="ApplyToPostponeClass_subtitle">강사이름</span>
+          <input className="applyToPostponeInput" type="text" placeholder="강사이름 선택"></input>
+        </Div>
+        <Div className="ApplyToPostponeClass_row">
+          <span className="ApplyToPostponeClass_subtitle">연기 사유 및 비고</span>
+          <input
+            className="applyToPostponeInput"
+            type="text"
+            placeholder="(선택) 연기사유를 자유롭게 작성해주세요."
+          ></input>
+        </Div>
       </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">강사이름</span>
-        <input type="text" placeholder="강사이름 선택"></input>
-      </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">연기 사유 및 비고</span>
-        <input type="text" placeholder="(선택)연기사유를 자유롭게 작성해주세요."></input>
-      </Div>
+      <span className="ApplyToPostponeClass_info">
+        *수업유형당 연기 가능 회수: 주 2회 수업: 월 2회 / 주 3회 이상 수업: 월 3회
+      </span>
+      <button className="ApplyToPostponeClass_btn">연기신청</button>
     </Div>
-    <Div className="">*수업유형당 연기 가능 회수: 주 2회 수업: 월 2회 / 주 3회 이상 수업: 월 3회</Div>
-    <button className="myPayment_btn">연기신청</button>
+    <div></div>
   </Div>
 );
 
--- src/components/organims/MyPage/MyPaymentDetails.tsx
+++ src/components/organims/MyPage/MyPaymentDetails.tsx
@@ -16,31 +16,35 @@ const MyPaymentDetails: React.FunctionComponent<Props> = ({
   pay_amount,
   pay_status,
 }: Props) => (
-  <Div className="MyPaymentDetails">
-    <Div className="myPayment_title" text="결제내역"></Div>
-    <Div className="myPayment_box">
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">결제번호</span>
-        <span className="myPayment_info">{pay_number}</span>
+  <Div className="MyPaymentDetails_form">
+    <div></div>
+    <Div className="MyPaymentDetails">
+      <Div className="myPayment_title" text="결제내역"></Div>
+      <Div className="myPayment_box">
+        <Div className="myPayment_col">
+          <span className="myPayment_subtitle">결제번호</span>
+          <span className="myPayment_info">{pay_number}</span>
+        </Div>
+        <Div className="myPayment_col">
+          <span className="myPayment_subtitle">결제일자</span>
+          <span className="myPayment_info">{pay_date}</span>
+        </Div>
+        <Div className="myPayment_col">
+          <span className="myPayment_subtitle">결제수업</span>
+          <span className="myPayment_info">{pay_classType}</span>
+        </Div>
+        <Div className="myPayment_col">
+          <span className="myPayment_subtitle">결제금액(원)</span>
+          <span className="myPayment_info">{pay_amount}</span>
+        </Div>
+        <Div className="myPayment_col">
+          <span className="myPayment_subtitle">상태</span>
+          <span className="myPayment_info">{pay_status}</span>
+        </Div>
       </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">결제일자</span>
-        <span className="myPayment_info">{pay_date}</span>
-      </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">결제수업</span>
-        <span className="myPayment_info">{pay_classType}</span>
-      </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">결제금액(원)</span>
-        <span className="myPayment_info">{pay_amount}</span>
-      </Div>
-      <Div className="myPayment_col">
-        <span className="myPayment_subtitle">상태</span>
-        <span className="myPayment_info">{pay_status}</span>
-      </Div>
+      <button className="myPayment_btn">{`더보기 >`}</button>
     </Div>
-    <button className="myPayment_btn">{`더보기 >`}</button>
+    <div></div>
   </Div>
 );
 
