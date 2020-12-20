e278686ee511de6ed9eb83ea7a9610bc416b959d
gunwooko <kkw722@gmail.com>
2020-12-20 23:44:44 +0900

    [:art:] cleanup AuthForm.tsx

--- src/containers/Auth/AuthForm.tsx
+++ src/containers/Auth/AuthForm.tsx
@@ -1,5 +1,5 @@
-import { authService, dbService } from 'fbase';
-import React, { useEffect, useState } from 'react';
+import { authService } from 'fbase';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 const AuthForm: React.FunctionComponent = (): React.ReactElement => {
@@ -82,8 +82,18 @@ const AuthForm: React.FunctionComponent = (): React.ReactElement => {
       if (newAccount) {
         // create account
         data = await authService.createUserWithEmailAndPassword(email, password);
+
+        // // DB에 유저정보 만들기 및 저장
+        // dbService.collection('users').add({
+        //   userName,
+        //   email,
+        //   createdAt: Date.now(),
+        // });
+
+        // // 가입 인증메일 보내기
         // data.user?.sendEmailVerification();
         // alert('인증메일이 발송되었습니다.');
+
         // authService.signOut();
         // history.push('/');
       } else {
@@ -91,22 +101,11 @@ const AuthForm: React.FunctionComponent = (): React.ReactElement => {
         data = await authService.signInWithEmailAndPassword(email, password);
         history.push('/');
       }
-      // console.log(data);
-      // return () => setNewAccount(false);
+      console.log('data', data);
     } catch (error) {
       setError(error.message);
     }
   };
-
-  // useEffect(() => {
-  //   // DB에 유저정보 만들기 및 저장
-  //   dbService.collection('users').add({
-  //     userName,
-  //     email,
-  //     createdAt: Date.now(),
-  //   });
-  //   console.log('????');
-  // }, []);
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
