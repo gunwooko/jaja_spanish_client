a6559def429ac68cd51528c0998d2cf371e10ccd
gunwooko <kkw722@gmail.com>
2020-12-20 23:19:25 +0900

    [:hammer:] resolved useEffect cleanup function issue :)

--- src/Hooks/useLoggedInCheck.tsx
+++ src/Hooks/useLoggedInCheck.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { authService } from 'fbase';
-
 const useLoggedInCheck = (): typeof authService.currentUser | null | boolean => {
-  // const [init, setInit] = useState(false);
+  // eslint-disable-next-line
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -12,15 +12,13 @@ const useLoggedInCheck = (): typeof authService.currentUser | null | boolean => 
       } else {
         setIsLoggedIn(false);
       }
-      // setInit(true);
+      setInit(true);
     });
-  }, [isLoggedIn]);
-
-  // useEffect(() => {
-  //   // There is an error note => Warning: Can't perform a React state update on an unmounted component. That's why we have to use cleanupFunction :)
-  //   // Ref => https://velog.io/@ohgoodkim/-%EC%97%90%EB%9F%AC%EB%85%B8%ED%8A%B8-Cant-perform-a-React-state-update-on-an-unmounted-component
-  //   return () => setIsLoggedIn(false);
-  // }, []);
+    // cleanup function
+    return () => {
+      setInit(false);
+    };
+  }, []);
 
   return isLoggedIn;
 };
