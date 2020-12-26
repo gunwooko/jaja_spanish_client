d7855157449c2e042740bc55751c40825fa0f3e7
gunwooko <kkw722@gmail.com>
2020-12-26 15:52:12 +0900

    [:hammer:] update Auth-firestoreService - update deleted doc when user leave <deletedUser>

--- src/containers/Auth/SocialLogIn.tsx
+++ src/containers/Auth/SocialLogIn.tsx
@@ -5,44 +5,50 @@ import React from 'react';
 
 const SocialLogIn: React.FunctionComponent = () => {
   const onSocialClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const {
-      currentTarget: { name },
-    } = event;
+    try {
+      const {
+        currentTarget: { name },
+      } = event;
 
-    console.log(name);
+      console.log(name);
 
-    // eslint-disable-next-line
-    let provider: any;
-    if (name === 'google') {
-      provider = new firebaseInstance.auth.GoogleAuthProvider();
-    } else if (name === 'facebook') {
-      provider = new firebaseInstance.auth.FacebookAuthProvider();
-    }
+      // eslint-disable-next-line
+      let provider: any;
+      if (name === 'google') {
+        provider = new firebaseInstance.auth.GoogleAuthProvider();
+      } else if (name === 'facebook') {
+        provider = new firebaseInstance.auth.FacebookAuthProvider();
+      }
 
-    const data = await authService.signInWithPopup(provider);
-    console.log(data.operationType);
+      const data = await authService.signInWithPopup(provider);
+      console.log(data.operationType);
 
-    const alreadyUsed: any[] = [];
-    await dbService
-      .collection('users')
-      .doc(`${data.user?.email}`)
-      .get()
-      .then((eachField) => {
-        alreadyUsed.push(eachField.data());
-      });
+      // eslint-disable-next-line
+      const alreadyUsed: any[] = [];
+      await dbService
+        .collection('users')
+        .doc(`${data.user?.email}`)
+        .get()
+        .then((eachField) => {
+          alreadyUsed.push(eachField.data());
+        });
 
-    if (!alreadyUsed[0]) {
-      // 우선 해당 유저가 이미 가입 되어 있는지 확인한다. 없으면 유저정보 저장하기
-      const hoy = getTodayUtil();
-      dbService.collection('users').doc(`${data.user?.email}`).set({
-        userName: data.user?.displayName,
-        email: data.user?.email,
-        createdAt: hoy,
-        userEngName: '영문 이름을 적어주세요.',
-        point: 0,
-        loginWith: name,
-        userId: data.user?.uid,
-      });
+      if (!alreadyUsed[0]) {
+        // 우선 해당 유저가 이미 가입 되어 있는지 확인한다. 없으면 유저정보 저장하기
+        const hoy = getTodayUtil();
+        dbService.collection('users').doc(`${data.user?.email}`).set({
+          userName: data.user?.displayName,
+          email: data.user?.email,
+          createdAt: hoy,
+          userEngName: '영문 이름을 적어주세요.',
+          point: 0,
+          loginWith: name,
+          userId: data.user?.uid,
+        });
+      }
+    } catch (err) {
+      console.error(err.message);
+      alert(err.message);
     }
   };
 
