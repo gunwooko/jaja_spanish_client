134d0b9f09857daeae2b42447af2b2f08d1b471b
gunwooko <kkw722@gmail.com>
2021-01-29 15:19:44 +0900

    [:hammer: :nail_care:] add styles to ReviewRead, update reviews views

--- src/components/organims/Review/ReviewRead/ReviewLine.tsx
+++ src/components/organims/Review/ReviewRead/ReviewLine.tsx
@@ -1,6 +1,6 @@
 import Div from 'components/atoms/Div';
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface Props {
   title: string;
